0e575b877e9e98ab58efc66f13c23ff8a467fc93
fabio Araujo <fabio.lanza4@gmail.com>
2024-08-05 18:39:21 -0700

    feat: add barbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,16 @@ import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 import Image from "next/image";
 import banner01 from "../assets/banner01.png";
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { db } from "@/lib/prisma";
+import BarberShopItem from "@/components/BarberShopItem";
 
-export default function Home() {
+async function Home() {
+  const Barbershop = await db.barbershop.findMany();
+  console.log(Barbershop);
+
   return (
     <main className="">
       <Header />
@@ -20,10 +28,54 @@ export default function Home() {
           </Button>
         </div>
 
-        <div className="relative w-full h-[150px] mt-6 ">
-          <Image src={banner01} alt="banner" fill className="object-cover rounded-xl" />
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src={banner01}
+            alt="banner"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* APPOINTMENT==================== */}
+        <h2 className="mb-3 mt-6 text-xl font-bold">Appointments</h2>
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/* LEFT_SIDE */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-medium">Hair Cut</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-8 w-8">
+                  <AvatarImage
+                    src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+                    alt="user"
+                  />
+                </Avatar>
+                <p className="text-sm">Barber Shop Name</p>
+              </div>
+            </div>
+
+            {/* RIGHT_SIDE */}
+            <div className="flex flex-col items-center justify-center border-l border-solid px-8">
+              <p className="mb-1 text-sm">August</p>
+              <p className="mb-1 text-2xl">05</p>
+              <p className="text-sm">09:00 am</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        {/* RECOMMEND==================== */}
+        <h2 className="mb-3 mt-6 text-xl font-bold">Recommended</h2>
+        <div className="flex gap-4  overflow-auto [&::-webkit-scrollbar]:hidden">
+
+        {Barbershop.map((barbershop) => (
+          <BarberShopItem key={barbershop.id} barbershop={barbershop} />
+        ))}
         </div>
       </div>
     </main>
   );
 }
+
+export default Home;
---
+++ components/BarberShopItem.tsx
@@ -0,0 +1,45 @@
+import { Barbershop } from "@prisma/client";
+import React from "react";
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { Badge } from "./ui/badge";
+import { Star, StarIcon } from "lucide-react";
+
+interface BarbershopProps {
+  barbershop: Barbershop;
+}
+
+const BarberShopItem = ({ barbershop }: BarbershopProps) => {
+  return (
+    <Card className="min-w-[159px]">
+      <CardContent className="p-0 px-1 pt-1">
+        {/* IMAGE */}
+        <div className="relative h-[160px] w-full">
+          <Image
+            src={barbershop.imageUrl}
+            alt="banner"
+            fill
+            className="rounded-xl object-cover"
+          />
+
+          <Badge className="absolute left-2 top-2" variant="secondary">
+            <StarIcon size={12} className="fill-primary text-primary" />
+            <p className="ml-1 text-xs">5.0</p>
+          </Badge>
+        </div>
+
+        {/* TITLE */}
+        <div className="py-3">
+          <h3 className="truncate font-semibold">{barbershop.name}</h3>
+          <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+          <Button variant="secondary" className="mt-3 w-full">
+            Book
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BarberShopItem;
